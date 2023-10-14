9dcef5a4f3e937bd075a4376a10466f213f76772
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-14 09:48:11 +0300

    feat: Add display product list functionality

--- main.js
+++ main.js
@@ -2,36 +2,43 @@ import './style.css'
 
 
 const TOTAL_PRODUCTS = 461;
+//let products = 
+let currentPage = 1;
+const productsList = document.querySelector('.products-list');
 
-let currentPage=1;
-
-
-function fetchProducts (currentPage) {
+async function fetchProducts (currentPage) {
 const LIMIT_PER_PAGE = 24;
 const DEFAULT_PAGE = 1;
 
 const API_URL = `https://voodoo-sandbox.myshopify.com/products.json?limit=${LIMIT_PER_PAGE}&page=${currentPage |  DEFAULT_PAGE
 }`
 
-return fetch(API_URL)
-  .then((response) => {
+  try {
+    const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error(`Network response was not ok: ${response.status}`)
+      throw new Error(`Network response was not ok: ${response.status}`);
     }
-    return response.json()
-  })
-  .then((data) => {
-    return data
-  })
-  .catch((error) => {
-    throw error
-  })
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
 
 }
-fetchProducts(currentPage)
-  .then((data) => {
-    console.log(data)
+function addProductsToDOM(products, list) {
+  products.forEach((product) => {
+    list.innerHTML += `<li>${product.id}</li>`
   })
-  .catch((error) => {
-    console.error('Fetch error:', error)
-  })
+}
+async function fetchAndDisplayProducts(currentPage) {
+  try {
+    const data = await fetchProducts(currentPage)
+    addProductsToDOM(data.products, productsList)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+window.addEventListener('load', () => {
+    fetchAndDisplayProducts(currentPage)
+})
+
