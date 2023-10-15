f311bc80f291be40a0f2b7bcc7cfb9dd55d35b5d
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-15 11:01:58 +0300

    feat: Add pagination functionality

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ import './style.css'
 
 
 const TOTAL_PRODUCTS = 461;
-//let products = 
-let currentPage = 1;
+const DEFAULT_PAGE = 1;
+const LIMIT_PER_PAGE = 24;
+const TOTAL_PAGES = Math.ceil(TOTAL_PRODUCTS / LIMIT_PER_PAGE);
+const PAGINATION_STEP = 5;
 const productsList = document.querySelector('.products-list');
+const pagination = document.querySelector('.pagination');
+let currentPage = 1;
 
 async function fetchProducts (currentPage) {
-const LIMIT_PER_PAGE = 24;
-const DEFAULT_PAGE = 1;
 
 const API_URL = `https://voodoo-sandbox.myshopify.com/products.json?limit=${LIMIT_PER_PAGE}&page=${currentPage |  DEFAULT_PAGE
 }`
@@ -26,6 +28,7 @@ const API_URL = `https://voodoo-sandbox.myshopify.com/products.json?limit=${LIMI
 
 }
 function addProductsToDOM(products, list) {
+    list.innerHTML = ''
   products.forEach((product) => {
     list.innerHTML += `<li>${product.id}</li>`
   })
@@ -38,7 +41,91 @@ async function fetchAndDisplayProducts(currentPage) {
     console.error('Error:', error)
   }
 }
-window.addEventListener('load', () => {
-    fetchAndDisplayProducts(currentPage)
-})
+function getPagesList(start, end) {
+  let pages = [] 
+    for (let i = start; i <= end; i++) {
+      pages.push(i)
+    }
+  return pages
+}
 
+function displayPagination(start, end) {
+  const pages = getPagesList(start, end);
+  pagination.innerHTML = '';
+
+  
+  console.log(currentPage);
+
+    if (currentPage === DEFAULT_PAGE && currentPage <= 5) {
+      pages.forEach((page) => {
+        pagination.innerHTML += `<li class='page border rounded-full h-10 w-10 font-light text-lg flex items-center justify-center'>${page}</li>`
+      })
+
+      if (end <= TOTAL_PAGES - PAGINATION_STEP) {
+        pagination.innerHTML +=
+          '<li class="pagination-ellipsis-forward border rounded-full h-10 w-10 font-light text-lg flex items-center justify-center">...</li>'
+        pagination.innerHTML += `<li class='page border rounded-full h-10 w-10 font-light text-lg flex items-center justify-center'>${TOTAL_PAGES}</li>`
+      }
+    }
+  if (currentPage >=6 && currentPage <=15) {
+    pagination.innerHTML += `<li class='page border rounded-full h-10 w-10 font-light flex text-lg items-center justify-center'>${DEFAULT_PAGE}</li>`
+    pagination.innerHTML +=
+      '<li class="pagination-ellipsis-back border rounded-full h-10 w-10 font-light  flex text-lg items-center justify-center">...</li>'
+    pages.forEach((page) => {
+      pagination.innerHTML += `<li class='page border rounded-full h-10 w-10 font-light text-lg flex items-center justify-center'>${page}</li>`
+    })
+    pagination.innerHTML +=
+      '<li class="pagination-ellipsis-forward border rounded-full h-10 w-10 font-light text-lg flex items-center justify-center">...</li>'
+    pagination.innerHTML += `<li class='page border rounded-full h-10 w-10 font-light text-lg flex items-center justify-center'>${TOTAL_PAGES}</li>`
+  }
+  if (currentPage === 16 && currentPage <= 20) {
+     pagination.innerHTML += `<li class='page border rounded-full h-10 w-10 font-light flex text-lg items-center justify-center'>${DEFAULT_PAGE}</li>`
+     pagination.innerHTML +=
+       '<li class="pagination-ellipsis-back border rounded-full h-10 w-10 font-light  flex text-lg items-center justify-center">...</li>'
+     pages.forEach((page) => {
+       pagination.innerHTML += `<li class='page border rounded-full h-10 w-10 font-light text-lg flex items-center justify-center'>${page}</li>`
+     })
+  }  
+}
+
+function swapPages(direction) {
+  let  newStart;
+  let newEnd; 
+  if (direction === "forward") {
+    newStart = currentPage + PAGINATION_STEP
+    newEnd = newStart + PAGINATION_STEP - 1
+  }
+ if (direction === "back") {
+   newStart = currentPage - PAGINATION_STEP
+   newEnd = currentPage-1
+ }
+  if (newStart > TOTAL_PAGES || newStart < 1) {
+    return
+  }
+  currentPage = newStart
+  fetchAndDisplayProducts(currentPage)
+  displayPagination(newStart, newEnd)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchAndDisplayProducts(DEFAULT_PAGE)
+  displayPagination(DEFAULT_PAGE, PAGINATION_STEP)
+
+  document.querySelector('.pagination').addEventListener('click', (event) => {
+    if (event.target.classList.contains('pagination-ellipsis-forward')) {
+      swapPages("forward")
+    }
+    if (event.target.classList.contains('pagination-ellipsis-back')) {
+      swapPages("back")
+      console.log("its not work")
+    }
+    if (event.target.classList.contains('page')) {
+      const clickedPage = parseInt(event.target.textContent, 10)
+      if (clickedPage >= DEFAULT_PAGE && clickedPage <= TOTAL_PAGES) {
+        currentPage = clickedPage
+        fetchAndDisplayProducts(currentPage)
+        console.log(currentPage)
+      }
+    }
+  })
+})
