21ac588ed5b5ab6057ad4c469ba3495997539c06
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-16 19:36:09 +0300

    bugfix: Try to fix ims bug

--- custom-disclosure-element.js
+++ custom-disclosure-element.js
@@ -1,62 +1,30 @@
 class CustomDisclosure extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.attractionimg = './assets/alert-circle.png';
-        this.arrowimg = './assets/chevron-down.png';
-        this.shadowRoot.innerHTML = `
-            <style>
-              .products-info {
-                background-color: black;
-                color: #FCF7E6;
-                border-radius: 4px;
-                padding: 1rem;
-                margin-bottom: 3rem;
-                max-width: 100%;
-                font-family: 'Space Grotesk', sans-serif;
-              }
-              .summary {
-                display: flex;
-                gap: 12px;
-              }
-              .attention-sign {
-                display: flex;
-                gap:2px; 
-                justify-content: center;
-                align-items: center;
-              }
-              .alert-circle {
-                margin-right: 4px;
-              }
-              .alpha {
-                font-size: 1rem;
-                font-weight: 700;
-              }
-              .message  {
-                font-size: 500;
-                font-size: 0.75rem;
-                padding-top: 0.25rem;
-                flex-grow: 1;
-              }
-              .arrow-down {
-                cursor: pointer;
-              }
-            </style>
-            <details class="products-info">
-                <summary class="summary">
-                    <div class="attention-sign">
-                        <img class="alert-circle" src=${this.attractionimg} alt="alert circle">
-                        <span class="alpha">ALPHA</span>
-                    </div>
-                    <p class="message">Important info regarding our service</p>
-                    <img class="arrow-down" src=${this.arrowimg} alt="arrow down">
-                </summary>
-                <p>
-                    <slot></slot>
-                </p>
-            </details>
-        `
-    }
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        /* CSS styles here */
+      </style>
+      <details class="products-info">
+        <summary class="summary">
+          <div class="attention-sign">
+            <img class="alert-circle" src="${this.attractionimg}" alt="alert circle">
+            <span class="alpha">ALPHA</span>
+          </div>
+          <p class="message">Important info regarding our service</p>
+          <img class="arrow-down" src="${this.arrowimg}" alt="arrow down">
+        </summary>
+        <p>
+          <slot></slot>
+        </p>
+      </details>
+    `
+
+    this.attractionimg = './assets/alert-circle.png'
+    this.arrowimg = './assets/chevron-down.png'
+  }
 }
 
 customElements.define('custom-disclosure', CustomDisclosure)
